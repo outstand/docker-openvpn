8687d8c5ea50d073912c902a4b6282189cb3c608
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-10-21 10:26:02 -0700

    Don't set up locking until we know we want it

--- vpn/docker-entrypoint.sh
+++ vpn/docker-entrypoint.sh
@@ -17,7 +17,8 @@ _no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
 _prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
 
 # ON START
-_prepare_locking
+# ryansch: moved _prepare_locking to conditional locking below
+#_prepare_locking
 
 # PUBLIC
 exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
@@ -29,7 +30,7 @@ unlock()            { _lock u; }   # drop a lock
 
 if [ "$1" = 'ovpn_run' ]; then
   echo 'Checking for config sync...'
-
+  _prepare_locking
   exlock
 
   until [ -f /etc/openvpn/lock/synced ]; do
